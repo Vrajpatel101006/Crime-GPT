5d0eb3c0e761166c82d770bc8f20039c8336b120
professional Developper <225871770+progammer24052025-png@users.noreply.github.com>
2026-06-23 18:34:04 +0530

    set

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     allowedHosts: [
       "postmeningeal-homeoplastic-janiyah.ngrok-free.dev",
-      "crime-gpt-v2.vercel.app"
+      "http://crime-gpt-v2.vercel.app"
     ]
   },
   build: {
